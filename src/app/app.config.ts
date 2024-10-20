ab38efc6c763e267a26c1d98014db8b0586bb689
michaljancar <117117284+michaljancar@users.noreply.github.com>
2024-10-20 03:49:12 +0200

    add MessageService to providers list

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,13 @@ import localeCs from '@angular/common/locales/cs';
 registerLocaleData(localeCs, 'cs');
 
 import { routes } from './app.routes';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     { provide: LOCALE_ID, useValue: 'cs-CZ' },
     provideAnimationsAsync(),
+    MessageService,
   ],
 };
